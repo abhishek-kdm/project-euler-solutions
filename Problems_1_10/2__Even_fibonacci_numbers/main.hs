1d2beff9e5ec54d60e9c32a32a4bb4d342101d1f
Abhishek <abhishekk19@gmail.com>
2019-09-07 19:58:25 +0530

    Problem 2 Even Fibonacci numbers.

---
+++ Problems_1_10/2__Even_fibonacci_numbers/main.hs
@@ -0,0 +1,8 @@
+fib :: [Int]
+fib = 1 : 2 : zipWith (+) fib (tail fib)
+
+solve :: Int -> Int
+solve n = sum . filter (\x -> x `mod` 2 == 0) . takeWhile (<= n) $ fib
+
+main = do
+  print . solve $ 4000000
