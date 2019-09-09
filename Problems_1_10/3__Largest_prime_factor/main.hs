4822607a26ff90908e2f769a8cb13820602b1e25
Abhishek <abhishekk19@gmail.com>
2019-09-09 14:20:16 +0530

    Problem 3 Largest prime factor.

---
+++ Problems_1_10/3__Largest_prime_factor/main.hs
@@ -0,0 +1,19 @@
+-- answer: 6857
+
+prime (x, y)
+  | isPrime x = (x, True)
+  | otherwise = (x, False)
+  where
+    isPrime n = length [x | x <- [2..(floor . sqrt . fromIntegral $ n)], n `mod` x == 0] == 0
+
+solve :: Int -> Int
+solve n = fst
+        . head
+        . dropWhile (not . snd)
+        . map prime
+        . filter (\(x, y) -> y == 0)
+        . map (divMod n) $ [2..]
+
+
+main = do
+  print . solve $ 600851475143
